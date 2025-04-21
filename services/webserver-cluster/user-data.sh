ff66f940dbc16c60e9767ae0e846c78211b09393
d-user11 <60410862+d-user11@users.noreply.github.com>
2025-04-21 12:16:35 +0300

    Show instance id on the web page

--- services/webserver-cluster/user-data.sh
+++ services/webserver-cluster/user-data.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+INSTANCE_ID=$(ec2metadata --instance-id)
+
 cat > index.html <<EOF
 <h1>${server_text}</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
+<p>instance_id: $INSTANCE_ID</p>
 EOF
 
 nohup busybox httpd -f -p ${server_port} &
