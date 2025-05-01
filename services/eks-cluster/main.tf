046f618e1aa56af237b8cbc57ca83593a62b14de
d-user11 <60410862+d-user11@users.noreply.github.com>
2025-05-01 15:07:17 +0300

    Change k8s version

--- services/eks-cluster/main.tf
+++ services/eks-cluster/main.tf
@@ -35,7 +35,7 @@ data "aws_subnets" "default" {
 resource "aws_eks_cluster" "cluster" {
   name     = var.name
   role_arn = aws_iam_role.cluster.arn
-  version  = "1.21"
+  version  = "1.32"
 
   vpc_config {
     subnet_ids = data.aws_subnets.default.ids
